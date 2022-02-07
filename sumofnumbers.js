499f9ce7b1049a3baab9d3e4e9718cf44e8a92b1
jeremiah-dy <97951461+jeremiah-dy@users.noreply.github.com>
2022-02-06 21:40:19 -1000

    Committed WOD

---
+++ sumofnumbers.js
@@ -0,0 +1,35 @@
+const test = [0, 1, 2, 3];
+
+function sumFor(list) {
+  let total = 0;
+  for (let i = 0; i < list.length; i++) {
+    total += list[i];
+  }
+  return total;
+}
+
+function sumWhile(list) {
+  let total = 0;
+  let i = 0;
+  while (i < list.length) {
+    total += list[i];
+    i++;
+  }
+  return total;
+}
+
+function sumRecursion(list) {
+  if (list.length === 0) {
+    return 0;
+  }
+  return list[0] + sumRecursion(list.slice(1, list.length));
+}
+
+function sumTheSimpleWay(list) {
+  return _.reduce(list, function (memo, num) { return memo + num; }, 0);
+}
+
+console.log(sumFor(test));
+console.log(sumWhile(test));
+console.log(sumRecursion(test));
+console.log(sumTheSimpleWay(test));
